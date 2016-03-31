4ceb4b5063ffa0270f7b45caf2889eb746e35b9f
tamami <tamami.oka@gmail.com>
2016-03-31 14:34:43 +0700

    perbaikan unit test pada modul core

--- src/lab/aikibo/test/UserManagerTest.java
+++ src/lab/aikibo/test/UserManagerTest.java
@@ -4,10 +4,17 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
 import lab.aikibo.entity.DatLogin;
+import lab.aikibo.hibernate.HibernateUtil;
+import lab.aikibo.hibernate.OpenSessionInViewListener;
 import lab.aikibo.manager.UserManager;
 
 public class UserManagerTest {
@@ -19,19 +26,24 @@ public class UserManagerTest {
 	@Before
 	public void init() {
 		um = new UserManager();
-		
+		HibernateUtil.getSessionPostgres().getCurrentSession().beginTransaction();
 	}
 	
 	@Test
 	public void testJumlahListUser() {
 		dataLogin = um.getListUser();
-		assertEquals(0, dataLogin.size());
+		assertEquals(1, dataLogin.size());
 	}
 	
 	@Test
 	public void testJumlahListUserString() {
 		data = um.getListUserInString();
-		assertEquals(0, data.size());
+		assertEquals(1, data.size());
+	}
+	
+	@After
+	public void finish() {
+		HibernateUtil.getSessionPostgres().getCurrentSession().close();
 	}
 
 }
