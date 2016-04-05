2a938e136902531248ebcc375fb6837bf69e6fc7
tamami <tamami.oka@gmail.com>
2016-04-05 20:58:31 +0700

    Masih kosong, kendala dengan koneksi 2 database

---
+++ src/lab/aikibo/manager/AksesManager.java
@@ -0,0 +1,11 @@
+package lab.aikibo.manager;
+
+import lab.aikibo.entity.RefAkses;
+
+public class AksesManager {
+	
+	public RefAkses getAkses(String user) {
+		return null; // ga bisa akses dari rumah.. db-nya udah di setting 2
+	}
+
+}
