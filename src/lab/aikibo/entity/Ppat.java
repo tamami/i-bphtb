e51e850615ac125904e0922eec598cce90a316cc
tamami <tamami.oka@gmail.com>
2016-04-09 09:04:29 +0700

    menambah alamat PPAT

--- src/lab/aikibo/entity/Ppat.java
+++ src/lab/aikibo/entity/Ppat.java
@@ -7,20 +7,32 @@ public class Ppat implements Serializable {
 	private static final long serialVersionUID = 8138026589058585312L;
 	private int kdPpat;
 	private String namaPpat;
+	private String alamatPpat;
 	
 	// --- setters and getters
 	
 	public int getKdPpat() {
 		return kdPpat;
 	}
+	
 	public void setKdPpat(int kdPpat) {
 		this.kdPpat = kdPpat;
 	}
+	
 	public String getNamaPpat() {
 		return namaPpat;
 	}
+	
 	public void setNamaPpat(String namaPpat) {
 		this.namaPpat = namaPpat;
+	}
+	
+	public String getAlamatPpat() {
+		return alamatPpat;
+	}
+	
+	public void setAlamatPpat(String alamatPpat) {
+		this.alamatPpat = alamatPpat;
 	}
 
 }
