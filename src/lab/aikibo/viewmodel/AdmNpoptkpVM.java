0e541f4344e75b46bfd240d9ef92f4b2935b537b
tamami <tamami.oka@gmail.com>
2016-04-16 22:06:42 +0700

    melengkapi fungsinya sebagai kelas ViewModel

--- src/lab/aikibo/viewmodel/AdmNpoptkpVM.java
+++ src/lab/aikibo/viewmodel/AdmNpoptkpVM.java
@@ -1,14 +1,65 @@
 package lab.aikibo.viewmodel;
 
+import java.util.List;
+
 import org.zkoss.bind.annotation.Init;
+
+import lab.aikibo.entity.RefNpoptkp;
+import lab.aikibo.manager.JenisPeralihanManager;
 
 public class AdmNpoptkpVM {
 	
-	
+	private RefNpoptkp currentNpoptkp;
+	private JenisPeralihanManager jpm;
+	private List<String> daftarJnsPeralihan;
+	private int indexJnsPeralihan;
 	
 	@Init
 	public void init() {
+		jpm = new JenisPeralihanManager();
 		
+		setDaftarJnsPeralihan(jpm.getAllDataFormatted());
+	}
+	
+	
+	// --- setters and getters
+
+	public RefNpoptkp getCurrentNpoptkp() {
+		return currentNpoptkp;
+	}
+
+	public JenisPeralihanManager getJpm() {
+		return jpm;
+	}
+
+
+	public void setJpm(JenisPeralihanManager jpm) {
+		this.jpm = jpm;
+	}
+
+
+	public void setCurrentNpoptkp(RefNpoptkp currentNpoptkp) {
+		this.currentNpoptkp = currentNpoptkp;
+	}
+
+	public List<String> getDaftarJnsPeralihan() {
+		return daftarJnsPeralihan;
+	}
+
+	public void setDaftarJnsPeralihan(List<String> daftarJnsPeralihan) {
+		this.daftarJnsPeralihan = daftarJnsPeralihan;
+	}
+
+
+	public int getIndexJnsPeralihan() {
+		indexJnsPeralihan = currentNpoptkp.getKdJnsPeralihan() - 1;
+		return indexJnsPeralihan;
+	}
+
+
+	public void setIndexJnsPeralihan(int indexJnsPeralihan) {
+		this.indexJnsPeralihan = indexJnsPeralihan;
+		currentNpoptkp.setKdJnsPeralihan(indexJnsPeralihan + 1);
 	}
 
 }
