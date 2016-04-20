a5b2235e8fbc46d8e5e159e2f9d479bde2f8b862
tamami <tamami.oka@gmail.com>
2016-04-20 20:23:48 +0700

    unit test untuk kelas NpoptkpManager

---
+++ src/lab/aikibo/test/NpoptkpManagerTest.java
@@ -0,0 +1,37 @@
+package lab.aikibo.test;
+
+
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import lab.aikibo.entity.RefNpoptkp;
+import lab.aikibo.hibernate.HibernateUtil;
+import lab.aikibo.manager.NpoptkpManager;
+
+public class NpoptkpManagerTest {
+	
+	private NpoptkpManager nm;
+	
+	@Before
+	public void before() {
+		nm = new NpoptkpManager();
+		HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+	}
+	
+	@Test
+	public void testData() {
+		List<RefNpoptkp> result = nm.getListAll();
+		org.junit.Assert.assertNotNull(result);
+	}
+	
+	
+	@After
+	public void finish() {
+		HibernateUtil.getSessionFactory().getCurrentSession().close();
+	}
+
+}
